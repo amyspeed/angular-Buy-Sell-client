c61e8cfc5e93e56c0804a5280c36f733ee818076
Amy Speed <amy.speed.henley@gmail.com>
2020-10-06 13:14:33 -0500

    initial commit

---
+++ src/app/edit-listing-page/edit-listing-page.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { fakeMyListings } from '../fake-data';
+import { Listing } from '../types';
+
+@Component({
+  selector: 'app-edit-listing-page',
+  templateUrl: './edit-listing-page.component.html',
+  styleUrls: ['./edit-listing-page.component.css']
+})
+export class EditListingPageComponent implements OnInit {
+  listing: Listing;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeMyListings.find(listing => listing.id === id);
+
+  }
+
+  onSubmit(): void {
+    alert('Saving changes...');
+    this.router.navigateByUrl('/my-listings');
+  }
+
+}
